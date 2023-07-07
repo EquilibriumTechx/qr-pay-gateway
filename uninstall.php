41fd677a03a6e9f05ad3749ca135cbe7c7936f84
EQUILIBRIUM SOLUTION (M) SDN. BHD <equilibrium.techx@gmail.com>
2023-07-08 00:58:06 +0800

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,12 +1,36 @@
-<?php // exit if uninstall constant is not defined
-// uninstall.php
+<?php
+// If uninstall.php is not called by WordPress, exit.
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-    exit;
+	exit;
 }
 
-// remove plugin options
-delete_option( 'title' );
-delete_option( 'description' );
-delete_option( 'instructions' );
-delete_option( 'order_status' );
-delete_option( 'media' );
+// Delete plugin options
+delete_option( 'tng_gateway_enabled' );
+delete_option( 'tng_gateway_title' );
+delete_option( 'tng_gateway_order_status' );
+delete_option( 'tng_gateway_description' );
+delete_option( 'tng_gateway_instructions' );
+delete_option( 'tng_gateway_media' );
+
+// Remove plugin meta data from orders
+$args = array(
+	'post_type'      => 'shop_order',
+	'posts_per_page' => -1,
+	'post_status'    => array( 'wc-pending', 'wc-processing', 'wc-on-hold', 'wc-completed' ),
+	'meta_query'     => array(
+		array(
+			'key'   => '_payment_method',
+			'value' => 'tng_gateway',
+		),
+	),
+);
+
+$orders = get_posts( $args );
+
+if ( $orders ) {
+	foreach ( $orders as $order ) {
+		delete_post_meta( $order->ID, 'full_pay_name' );
+		delete_post_meta( $order->ID, 'mobile' );
+		delete_post_meta( $order->ID, 'transaction' );
+	}
+}
