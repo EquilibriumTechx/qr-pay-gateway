b880b373805f392019e944c7904391a0e98f7ea9
EQUILIBRIUM SOLUTION (M) SDN. BHD <equilibrium.techx@gmail.com>
2023-07-08 00:20:26 +0800

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,9 +1,15 @@
 <?php // exit if uninstall constant is not defined
-if (!defined('WP_UNINSTALL_PLUGIN')) exit;
+// uninstall.php
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+    exit;
+}
 
 // remove plugin options
 delete_option( 'title' );
 delete_option( 'description' );
 delete_option( 'instructions' );
-delete_option( 'order_status');
+delete_option( 'order_status' );
 delete_option( 'media' );
+
+// Call the renamed uninstall function
+wc_tng_uninstall_plugin( __FILE__ );
